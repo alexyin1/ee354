db3bfdd125dd7ab8c2e533f962e8e60afbfc1bdf
thejonmonte <jmontema@usc.edu>
2020-04-20 22:04:49 -0700

    Worked more on score, collision detection, keeping track of time, SSDs

--- project/vga_project/vga_project.srcs/sources_1/imports/src/block_controller.v
+++ project/vga_project/vga_project.srcs/sources_1/imports/src/block_controller.v
@@ -1,6 +1,15 @@
+/* Problems:
+- 2's complement 10'b1111111011 AKA -5 doesn't work
+- Score increments by 2's
+- Timer doesn't correctly wait for 10 secs
+- SSD doesn't show the Qp3, Qp2, Qp1, Qi states on SSD4, but SSD5 shows Done state correctly
+*/
+
 `timescale 1ns / 1ps
 
-module block_controller(
+module block_controller
+#(parameter N_dc = 28)
+  (
 	input clk,
 	input bright,
 	input rst,
@@ -8,11 +17,15 @@ module block_controller(
 	input [9:0] hCount, vCount,
 	output reg [11:0] rgb,
 	output reg [3:0] score_ones,
-	output Qi, Qp1, Qp2, Qp3, Done
+	output reg [3:0] score_tens,
+	output reg [3:0] score_hundreds,
+	output Qi, Qp1, Qp2, Qp3, Done,
+	output reg ten_secs
    );
 	reg [1:0] direction;
 	reg [27:0]	DIV_CLK;
 	reg [4:0] state;
+	
 	always @ (posedge clk, posedge rst)  
 	begin : CLOCK_DIVIDER
       if (rst)
@@ -20,20 +33,30 @@ module block_controller(
 	  else
 			DIV_CLK <= DIV_CLK + 1'b1;
 	end
+
 	wire move_clk;
 	assign move_clk=DIV_CLK[19];
 	wire bird_fill;
 	wire block_fill;
 	integer i;
+
+	reg [N_dc-1:0] timer_count;
+	reg [3:0] check_ten_secs;
 	
 	//these two values dictate the center of the block, incrementing and decrementing them at a rate leads the block to move in certain directions
 	reg [9:0] bird_x, bird_y;
 	reg [9:0] block_x, block_y;
 	reg [9:0] sub_x, sub_y;
+
+	// wire feedback;
+	// assign feedback = block_x
+
+	reg direction_chosen, passed_ten_secs;
 	
-	parameter RED   = 12'b1111_0000_0000;
-	parameter GREEN = 12'b0000_1111_0000;
-	parameter BLUE  = 12'b0000_0000_1111;
+	localparam 
+	RED  = 12'b1111_0000_0000,
+	GREEN = 12'b0000_1111_0000,
+	BLUE  = 12'b0000_0000_1111;
 
 	localparam
 	INITIAL = 5'b00001,
@@ -64,7 +87,6 @@ module block_controller(
 	begin
 		if(rst)
 		begin 
-			//rough values for center of screen
 			state <= INITIAL;
 			bird_x <= 10'bXXXXXXXXXX;
 			bird_y <= 10'bXXXXXXXXXX;
@@ -73,6 +95,10 @@ module block_controller(
 			sub_x <= 10'bXXXXXXXXXX;
 			sub_y <= 10'bXXXXXXXXXX;
 			score_ones <= 4'bXXXX;
+			score_tens <= 4'bXXXX;
+			score_hundreds <= 4'bXXXX;
+			direction_chosen <= 1'bX;
+			passed_ten_secs <= 1'bX;
 		end
 		else 
 		begin
@@ -83,12 +109,24 @@ module block_controller(
 					if (Start) // Start is left button
 						state <= PHASE1;
 
+					//rough values for center of screen
 					bird_x<=200;
 					bird_y<=250;
+
 					block_x <= 700;
 					block_y <= 250;
-					sub_x <= 6;
-					sub_y <= 6;
+
+					// This only works for testbench I think... then how can we produce random positions for the blocks to be in?
+					// block_x <= $urandom_range(700, 600);
+					// block_y <= $urandom_range(505, 30);
+
+					sub_x <= -1;
+					sub_y <= -6;
+					score_ones <= 0;
+					score_tens <= 0;
+					score_hundreds <= 0;
+					direction_chosen <= 1'b0;
+					passed_ten_secs <= 1'b0;
 				end
 
 				PHASE1:
@@ -98,57 +136,182 @@ module block_controller(
 					pulse (called the back porch) that the scanning doesn't output to the display. So effectively, the top left corner corresponds 
 					to (hcount,vcount)~(144,35). Which means with a 640x480 resolution, the bottom right corner corresponds to ~(783,515).  
 					*/
-					// if(direction==3'b100)
-					// begin // right
-					// 	bird_x<=bird_x+2; //change the amount you increment to make the speed faster 
-					// 	if(bird_x==800) //these are rough values to attempt looping around, you can adjust them to make it more accurate- refer to the block comment above
-					// 		bird_x<=150;
+
+					// For the block, we want to choose a random position between
+					// 30 and 505 vertically
+					// 810 and 1010 horizontally
+
+					// passed_ten_secs <= 1'b0;
+					direction_chosen <= 1'b0;
+
+					// if (ten_secs == 1'b1)
+					// begin
+					// 	passed_ten_secs <= 1'b1;
+					// 	state <= PHASE2;
 					// end
-					// else if(direction==3'b101) 
-					// begin // left
-					// 	bird_x<=bird_x-2; 
-					// 	if(bird_x==150)
-					// 		bird_x<=800;
-					// end
-					if(direction==2'b10) 
+
+					if(direction == 2'b10)
 					begin // up
-						bird_y<=bird_y-2;
-						if(bird_y==34)
+						bird_y<=bird_y-10;
+						if(bird_y<=34)
 							bird_y<=514;
+						direction_chosen <= 1'b1; // Tell the full clk that we moved up and to reset direction back to zero.
 					end
-					else if(direction==2'b11) 
+					else if(direction == 2'b11)
 					begin // down
-						bird_y<=bird_y+2;
-						if(bird_y==514)
+						bird_y<=bird_y+10;
+						if(bird_y>=514)
 							bird_y<=34;
+						direction_chosen <= 1'b1; // Tell the full clk that we moved down and reset direction back to zero.
 					end
 
 					block_x <= block_x - 1;
-					if (block_x == 150) 
+					if (block_x <= 150) 
 					begin
 						block_x <= 800;
 					end
 
 					sub_x <= block_x - bird_x;
 					sub_y <= block_y - bird_y;
-					if (sub_x >= 0 && sub_x <= 5 && sub_y >= 0 && sub_y <= 5)
+
+					if (sub_x >= 0 && sub_x <= 10 && sub_y >= 0 && sub_y <= 10)
 					begin
-						score_ones <= score_ones + 1;
 						bird_x <= 200;
 						bird_y <= 250;
+						block_x <= 700;
+						block_y <= 250;
+
+						score_ones <= score_ones + 1;
+						if (score_ones == 9)
+						begin
+							score_ones <= 0;
+							score_tens <= score_tens + 1;
+						end
+						if (score_tens == 9)
+						begin
+							score_ones <= 0;
+							score_tens <= 0;
+							score_hundreds <= score_hundreds + 1;
+						end
 					end
+
 				end
 
 				PHASE2:
 				// Blocks move faster. This is reached after 10 seconds.
 				begin
-					state <= PHASE3;
+					passed_ten_secs <= 1'b0;
+					direction_chosen <= 1'b0;
+					
+					if (ten_secs == 1'b1)
+					begin
+						passed_ten_secs <= 1'b1;
+						state <= PHASE3;
+					end
+
+					if(direction == 2'b10) 
+					begin // up
+						bird_y<=bird_y-10;
+						if(bird_y<=34)
+							bird_y<=514;
+						direction_chosen <= 1'b1;
+					end
+					else if(direction == 2'b11) 
+					begin // down
+						bird_y<=bird_y+10;
+						if(bird_y>=514)
+							bird_y<=34;
+						direction_chosen <= 1'b1;
+					end
+
+					block_x <= block_x - 3;
+					if (block_x <= 150) 
+					begin
+						block_x <= 800;
+					end
+
+					sub_x <= block_x - bird_x;
+					sub_y <= block_y - bird_y;
+					// 2's complement 10'b1111111011 AKA -5 doesn't work
+					// Score increments by 2's
+					if (sub_x >= 0 && sub_x <= 10 && sub_y >= 0 && sub_y <= 10)
+					begin
+						bird_x <= 200;
+						bird_y <= 250;
+						block_x <= 700;
+						block_y <= 250;
+
+						score_ones <= score_ones + 1;
+						if (score_ones == 9)
+						begin
+							score_ones <= 0;
+							score_tens <= score_tens + 1;
+						end
+						if (score_tens == 9)
+						begin
+							score_ones <= 0;
+							score_tens <= 0;
+							score_hundreds <= score_hundreds + 1;
+						end
+					end
+
 				end
 
 				PHASE3:
 				// Blocks move faster. This is reached after 20 seconds.
 				begin
-					state <= DONE;
+					passed_ten_secs <= 1'b0;
+					direction_chosen <= 1'b0;
+
+					if (ten_secs == 1'b1)
+					begin
+						passed_ten_secs <= 1'b1;
+						state <= DONE;
+					end
+
+					if(direction == 2'b10) 
+					begin // up
+						bird_y<=bird_y-10;
+						if(bird_y<=34)
+							bird_y<=514;
+						direction_chosen <= 1'b1;
+					end
+					else if(direction == 2'b11) 
+					begin // down
+						bird_y<=bird_y+10;
+						if(bird_y>=514)
+							bird_y<=34;
+						direction_chosen <= 1'b1;
+					end
+
+					block_x <= block_x - 5;
+					if (block_x <= 150) 
+					begin
+						block_x <= 800;
+					end
+
+					sub_x <= block_x - bird_x;
+					sub_y <= block_y - bird_y;
+					if (sub_x >= 0 && sub_x <= 10 && sub_y >= 0 && sub_y <= 10)
+					begin
+						bird_x <= 200;
+						bird_y <= 250;
+						block_x <= 700;
+						block_y <= 250;
+
+						score_ones <= score_ones + 1;
+						if (score_ones == 9)
+						begin
+							score_ones <= 0;
+							score_tens <= score_tens + 1;
+						end
+						if (score_tens == 9)
+						begin
+							score_ones <= 0;
+							score_tens <= 0;
+							score_hundreds <= score_hundreds + 1;
+						end
+					end
 				end
 
 				DONE:
@@ -160,15 +323,38 @@ module block_controller(
 		end
 	end
 	
-	//msb of direction acts as a sort of enable, lower two bits dictate direction
+	//msb of direction acts as a sort of enable, lower bits dictate direction
 	always@(posedge clk,posedge rst) begin
 		if(rst)
+		begin
 			direction<=2'b00;
-		else begin
+			ten_secs <= 1'b0;
+		end
+		else 
+		begin
 			if(up)
 				direction<=2'b10;
 			else if(down)
 				direction<=2'b11;
+
+			if (direction_chosen)
+				direction<=2'b00;
+			
+			timer_count <= timer_count + 1;
+
+			if (timer_count[N_dc-1])
+			begin
+				check_ten_secs <= check_ten_secs + 1;
+				if (check_ten_secs == 9)
+				begin
+					check_ten_secs <= 0;
+					timer_count <= 0;
+					ten_secs <= 1'b1;
+				end 
+			end
+
+			if (passed_ten_secs)
+				ten_secs <= 1'b0;
 		end
 	end
 	
--- project/vga_project/vga_project.srcs/sources_1/imports/src/vga_top.v
+++ project/vga_project/vga_project.srcs/sources_1/imports/src/vga_top.v
@@ -53,23 +53,24 @@ module vga_top(
 	wire Qi, Qp1, Qp2, Qp3, Done;
 
 	wire [3:0] score_ones;
+	wire [3:0] score_tens;
+	wire [3:0] score_hundreds;
+	
+	wire ten_secs;
 
-	wire [3:0] SSD0, SSD1, SSD2, SSD3;	
-	reg [6:0] SSD_CATHODES;
+	wire [3:0] SSD0, SSD1, SSD2, SSD3, SSD4, SSD5, SSD6, SSD7;	
+	reg [7:0] SSD_CATHODES;
 	reg [3:0] SSD;
 
-	wire [1:0] ssdscan_clk;
+	wire [2:0] ssdscan_clk;
 	reg [26:0] DIV_CLK;
 	wire board_clk;
 
-	ee354_debouncer #(.N_dc(28)) ee354_debouncer_1 (.CLK(ClkPort), .RESET(Reset), .PB(BtnL), .SCEN(left));
-	ee354_debouncer #(.N_dc(28)) ee354_debouncer_2 (.CLK(ClkPort), .RESET(Reset), .PB(BtnR), .SCEN(right));
-	ee354_debouncer #(.N_dc(28)) ee354_debouncer_3 (.CLK(ClkPort), .RESET(Reset), .PB(BtnU), .SCEN(up));
-	ee354_debouncer #(.N_dc(28)) ee354_debouncer_4 (.CLK(ClkPort), .RESET(Reset), .PB(BtnD), .SCEN(down));
+	ee354_debouncer #(.N_dc(28)) ee354_debouncer_up (.CLK(ClkPort), .RESET(Reset), .PB(BtnU), .SCEN(up));
+	ee354_debouncer #(.N_dc(28)) ee354_debouncer_down (.CLK(ClkPort), .RESET(Reset), .PB(BtnD), .SCEN(down));
 	display_controller dc(.clk(ClkPort), .hSync(hSync), .vSync(vSync), .bright(bright), .hCount(hc), .vCount(vc));
 	block_controller sc(.clk(ClkPort), .bright(bright), .rst(Reset), .up(up), .down(down),.Start(Start),.Ack(Ack),.hCount(hc), .vCount(vc), .rgb(rgb), .score_ones(score_ones),
-	.Qi(Qi), .Qp1(Qp1), .Qp2(Qp2), .Qp3(Qp3), .Done(Done) );
-
+	.score_tens(score_tens), .score_hundreds(score_hundreds), .Qi(Qi), .Qp1(Qp1), .Qp2(Qp2), .Qp3(Qp3), .Done(Done), .ten_secs(ten_secs) );
 	assign board_clk = ClkPort;
 
 	always @ (posedge board_clk, posedge Reset)  
@@ -87,51 +88,66 @@ module vga_top(
 	// disable mamory ports
 	assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
 
-	assign SSD0 = score_ones;
-
-	assign ssdscan_clk = DIV_CLK[19:18];
-	
-	assign An0	= !(~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 00
-	assign An1	= !(~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 01
-	assign An2	= !( (ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 10
-	assign An3	= !( (ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 11
-	
+	// Output: LEDs
 	assign {Ld7, Ld6, Ld5, Ld4, Ld3} = {Qi, Qp1, Qp2, Qp3, Done};
 	assign {Ld2, Ld1, Ld0} = {Start, Ack, Reset};
 
-	always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3)
+	// SSDs
+	assign SSD5 = {1'b0, 1'b0, 1'b0, Done};
+	assign SSD4 = {Qp3, Qp2, Qp1, Qi};
+	assign SSD2 = score_hundreds;
+	assign SSD1 = score_tens;
+	assign SSD0 = score_ones;
+
+	assign ssdscan_clk = DIV_CLK[19:17];
+	
+	assign An0	= !(~(ssdscan_clk[2]) && ~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 000
+	assign An1	= !(~(ssdscan_clk[2]) && ~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 001
+	assign An2	= !(~(ssdscan_clk[2]) &&  (ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 010
+	assign An3	= !(~(ssdscan_clk[2]) &&  (ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 011
+	assign An4	= !( (ssdscan_clk[2]) &&  (ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 100
+	assign An5	= !( (ssdscan_clk[2]) && ~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 101
+	assign An6	= !( (ssdscan_clk[2]) &&  (ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 110
+	assign An7	= !( (ssdscan_clk[2]) &&  (ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 111
+
+	// SSD Scanning
+	always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3, SSD4, SSD5, SSD6, SSD7)
 	begin : SSD_SCAN_OUT
 		case (ssdscan_clk) 
-		 	2'b00: SSD = SSD0;
-		 	2'b01: SSD = SSD1;
-		 	2'b10: SSD = SSD2;
-		 	2'b11: SSD = SSD3;
+		 	3'b000: SSD = SSD0;
+		 	3'b001: SSD = SSD1;
+		 	3'b010: SSD = SSD2;
+		 	3'b011: SSD = SSD3;
+			3'b100: SSD = SSD4;
+		 	3'b101: SSD = SSD5;
+		 	3'b110: SSD = SSD6;
+		 	3'b111: SSD = SSD7;
 		endcase
 	end	
 
-	// Hex-to-SSD conversion
+	// Following is Hex-to-SSD conversion
 	always @ (SSD) 
 	begin : HEX_TO_SSD
 		case (SSD)
-			4'b0000: SSD_CATHODES = 7'b0000001 ; // 0
-			4'b0001: SSD_CATHODES = 7'b1001111 ; // 1
-			4'b0010: SSD_CATHODES = 7'b0010010 ; // 2
-			4'b0011: SSD_CATHODES = 7'b0000110 ; // 3
-			4'b0100: SSD_CATHODES = 7'b1001100 ; // 4
-			4'b0101: SSD_CATHODES = 7'b0100100 ; // 5
-			4'b0110: SSD_CATHODES = 7'b0100000 ; // 6
-			4'b0111: SSD_CATHODES = 7'b0001111 ; // 7
-			4'b1000: SSD_CATHODES = 7'b0000000 ; // 8
-			4'b1001: SSD_CATHODES = 7'b0000100 ; // 9
-			4'b1010: SSD_CATHODES = 7'b0001000 ; // A
-			4'b1011: SSD_CATHODES = 7'b1100000 ; // B
-			4'b1100: SSD_CATHODES = 7'b0110001 ; // C
-			4'b1101: SSD_CATHODES = 7'b1000010 ; // D
-			4'b1110: SSD_CATHODES = 7'b0110000 ; // E
-			4'b1111: SSD_CATHODES = 7'b0111000 ; // F    
-			default: SSD_CATHODES = 7'bXXXXXXX ; // default is not needed as we covered all cases
+			4'b0000: SSD_CATHODES = 8'b00000011; // 0
+			4'b0001: SSD_CATHODES = 8'b10011111; // 1
+			4'b0010: SSD_CATHODES = 8'b00100101; // 2
+			4'b0011: SSD_CATHODES = 8'b00001101; // 3
+			4'b0100: SSD_CATHODES = 8'b10011001; // 4
+			4'b0101: SSD_CATHODES = 8'b01001001; // 5
+			4'b0110: SSD_CATHODES = 8'b01000001; // 6
+			4'b0111: SSD_CATHODES = 8'b00011111; // 7
+			4'b1000: SSD_CATHODES = 8'b00000001; // 8
+			4'b1001: SSD_CATHODES = 8'b00001001; // 9
+			4'b1010: SSD_CATHODES = 8'b00010001; // A
+			4'b1011: SSD_CATHODES = 8'b11000001; // B
+			4'b1100: SSD_CATHODES = 8'b01100011; // C
+			4'b1101: SSD_CATHODES = 8'b10000101; // D
+			4'b1110: SSD_CATHODES = 8'b01100001; // E
+			4'b1111: SSD_CATHODES = 8'b01110001; // F    
+			default: SSD_CATHODES = 8'bXXXXXXXX; // default is not needed as we covered all cases
 		endcase
-	end
+	end	
 
 	assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp} = {SSD_CATHODES};	
 
